c3168179a2a81d548c0be37e3a247b2ded17f48c
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-12-01 23:47:15 +0100

    Run formatter for all files

--- lib/advent_of_code_2018.ex
+++ lib/advent_of_code_2018.ex
@@ -6,6 +6,6 @@ defmodule AdventOfCode2018 do
   def input!(path) do
     File.stream!(path)
     |> Stream.map(&String.trim_trailing/1)
-    |> Enum.to_list
+    |> Enum.to_list()
   end
 end
--- lib/advent_of_code_2018/day_01.ex
+++ lib/advent_of_code_2018/day_01.ex
@@ -1,6 +1,6 @@
 defmodule AdventOfCode2018.Day01 do
   def part1(args) do
-    args |> Enum.map(&parse/1) |> Enum.sum
+    args |> Enum.map(&parse/1) |> Enum.sum()
   end
 
   defp parse(value) do
@@ -11,11 +11,13 @@ defmodule AdventOfCode2018.Day01 do
   end
 
   def part2(args) do
-    members = MapSet.put(MapSet.new, 0)
+    members = MapSet.put(MapSet.new(), 0)
+
     Stream.cycle(args)
     |> Stream.map(&parse/1)
     |> Stream.transform({members, 0}, fn change, {members, frequency} ->
       current = frequency + change
+
       if MapSet.member?(members, current) do
         send(self(), current)
         {:halt, current}
@@ -23,7 +25,7 @@ defmodule AdventOfCode2018.Day01 do
         {[], {MapSet.put(members, current), current}}
       end
     end)
-    |> Stream.run
+    |> Stream.run()
 
     receive do
       x -> x
