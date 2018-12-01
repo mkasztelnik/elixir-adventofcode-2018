09ae5f211fec039d24a7ece0f78c59d68ccba2cd
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-12-01 23:45:28 +0100

    Use describe in unit tests

--- test/advent_of_code_2018/day_01_test.exs
+++ test/advent_of_code_2018/day_01_test.exs
@@ -3,53 +3,57 @@ defmodule AdventOfCode2018.Day01Test do
 
   import AdventOfCode2018.Day01
 
-  test "part1 test case 1" do
-    input = input!("+1, +1, +1")
-    result = part1(input)
+  describe "part1/1" do
+    test "case 1" do
+      input = input!("+1, +1, +1")
+      result = part1(input)
 
-    assert result == 3
+      assert result == 3
+    end
+
+    test "case 2" do
+      input = input!("+1, +1, -2")
+      result = part1(input)
+
+      assert result == 0
+    end
+
+    test "case 3" do
+      input = input!("-1, -2, -3")
+      result = part1(input)
+
+      assert result == -6
+    end
   end
 
-  test "part1 test case 2" do
-    input = input!("+1, +1, -2")
-    result = part1(input)
+  describe "part2/1" do
+    test "case 1" do
+      input = input!("+1, -1")
+      result = part2(input)
 
-    assert result == 0
-  end
+      assert result == 0
+    end
 
-  test "part1 test case 3" do
-    input = input!("-1, -2, -3")
-    result = part1(input)
+    test "case 2" do
+      input = input!("+3, +3, +4, -2, -4")
+      result = part2(input)
 
-    assert result == -6
-  end
+      assert result == 10
+    end
 
-  test "part2 test case 1" do
-    input = input!("+1, -1")
-    result = part2(input)
+    test "case 3" do
+      input = input!("-6, +3, +8, +5, -6")
+      result = part2(input)
 
-    assert result == 0
-  end
+      assert result == 5
+    end
 
-  test "part2 test case 2" do
-    input = input!("+3, +3, +4, -2, -4")
-    result = part2(input)
+    test "case 4" do
+      input = input!("+7, +7, -2, -7, -4")
+      result = part2(input)
 
-    assert result == 10
-  end
-
-  test "part2 test case 3" do
-    input = input!("-6, +3, +8, +5, -6")
-    result = part2(input)
-
-    assert result == 5
-  end
-
-  test "part2 test case 4" do
-    input = input!("+7, +7, -2, -7, -4")
-    result = part2(input)
-
-    assert result == 14
+      assert result == 14
+    end
   end
 
   def input!(str) do
